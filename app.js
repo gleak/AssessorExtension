e44dbc88629a981215160b006e27c54f73f74554
Antonio Molinari <molinari.antonio@hotmail.it>
2021-08-08 12:21:00 +0200

    fixx css and creation of object only on load complete

--- app.js
+++ app.js
@@ -1,37 +1,39 @@
-console.log("Load");
+document.addEventListener('load',function(){
 
-const controller = new ExtensionSeleniumController('{a6fd85ed-e919-4a43-a5af-8da18bda539f}'); //Firefox KEY
-controller.activateListenerHealth();
+  console.log("Load");
 
-const view = new ExtensionSeleniumView(controller);
+  const controller = new ExtensionSeleniumController('{a6fd85ed-e919-4a43-a5af-8da18bda539f}'); //Firefox KEY
+  controller.activateListenerHealth();
 
-document.addEventListener('keydown',(event)=>{
-  let name = event.key;
-  switch(name){
-    case "F8":
-      openModalPO();
-    break;
-    case "F4":
-      stopRecordingPO();
-    break;
-    default:
-      console.log(name);
+  const view = new ExtensionSeleniumView(controller);
+
+  document.addEventListener('keydown',(event)=>{
+    let name = event.key;
+    switch(name){
+      case "F8":
+        openModalPO();
+      break;
+      case "F4":
+        stopRecordingPO();
+      break;
+      default:
+        console.log(name);
+    }
+    
+  },false);
+
+  function openModalPO(){
+    let checkRecording = document.getElementById('selenium-ide-indicator');
+    if(checkRecording==null){
+      alert("La registrazione non è attiva!");
+      return;
+    }  
+    view.openModal();
   }
-  
-},false);
 
-function openModalPO(){
-  let checkRecording = document.getElementById('selenium-ide-indicator');
-  if(checkRecording==null){
-    alert("La registrazione non è attiva!");
-    return;
-  }  
-  view.openModal();
-}
+  function stopRecordingPO(){
+    controller.stopRecordPO();
+  }
 
-function stopRecordingPO(){
-	controller.stopRecordPO();
-}
-
-console.log("Init Complete");
-  
+  console.log("Init Complete");
+});
